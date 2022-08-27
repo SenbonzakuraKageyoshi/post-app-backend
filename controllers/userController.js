4ee75116b22fbbdff522c981fe59d6a04f09f937
SenbonzakuraKageyoshi <kacceldev@gmail.com>
2022-08-27 18:28:14 +0300

    Лайки юзера приходят теперь и при логине

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,9 +37,11 @@ const login = async (req, res) => {
             return res.status(404).json({message: 'Неверный логин или пароль'});
         };
 
+        const userLikes = await UserLike.findAll({ where: {UserId: user.dataValues.id}});
+
         const token = generateToken(user.dataValues.id)
 
-        res.json({...user.dataValues, token});
+        res.json({...user.dataValues, likes: userLikes, token});
     } catch (error) {   
         console.log(error)
         res.json({message: 'Ошибка входа'})
@@ -58,7 +60,6 @@ const getMe = async (req, res) => {
 
         const user = await User.findOne({ where: {id}});
         const userLikes = await UserLike.findAll({ where: {UserId: id}});
-        console.log(id)
 
         res.json({...user.dataValues, likes: userLikes});
 
@@ -68,6 +69,26 @@ const getMe = async (req, res) => {
     }
 };
 
+const getLikedPosts = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+
+        if(!token){
+            return res.status(404).json(null);
+        }
+
+        const { id } = jwt.verify(token, '1a2b-3c4d-5e6f-7g8h');
+
+        const userLikes = await UserLike.findAll({ where: {UserId: id}});
+
+        res.json(userLikes);
+
+    } catch (error) {   
+        console.log(error)
+        res.json(null)
+    }
+}
+
 export {
-    register, login, getMe
+    register, login, getMe, getLikedPosts
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { register, login, getMe } from "../controllers/userController.js";
+import authCheck from "../utils/authCheck.js";
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/me', getMe);
+router.post('/user-likes', authCheck, getMe);
 
 export default router;
